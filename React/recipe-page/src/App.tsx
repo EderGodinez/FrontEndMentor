b9059ec3666c95751a4283160470f74a352d4e5d
EderGodinez <eder.godinez9511@alumnos.udg.mx>
2024-06-24 22:54:01 -0600

    Challenge #2 recipe page created

---
+++ React/recipe-page/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Recipe from './components/recipe'
+function App() {
+
+  return (
+    <>
+    <div className='main_container'>
+    <Recipe />
+    </div>
+    
+    </>
+  )
+}
+
+export default App
